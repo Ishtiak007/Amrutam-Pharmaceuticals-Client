e17fbb1267f03ba18deb5acacb85fb1ee396daaa
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-04-19 12:25:28 +0600

    Consultation section added to the home page

--- src/Pages/Home/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home/Home.jsx
@@ -1,7 +1,9 @@
 
 import AfterBannerSection from "../AfterBanner/AfterBannerSection";
 import Banner from "../Banner/Banner";
+import ConsultationsSec from "../ConsultationsSec/ConsultationsSec";
 import FeaturedSection from "../FeaturedSection/FeaturedSection";
+import HomeAppSec from "../HomeAppSec/HomeAppSec";
 
 const Home = () => {
     return (
@@ -9,6 +11,8 @@ const Home = () => {
             <Banner></Banner>
             <AfterBannerSection></AfterBannerSection>
             <FeaturedSection></FeaturedSection>
+            <ConsultationsSec></ConsultationsSec>
+            <HomeAppSec></HomeAppSec>
         </div>
     );
 };
---
+++ src/Pages/Home/Home/HomeAppSec/HomeAppSec.jsx
@@ -0,0 +1,12 @@
+import phoneApp from '../../../../assets/phnApp.png'
+
+const HomeAppSec = () => {
+    return (
+        <div>
+
+
+        </div>
+    );
+};
+
+export default HomeAppSec;
