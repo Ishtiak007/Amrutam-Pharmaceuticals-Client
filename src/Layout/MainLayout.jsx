8354fc46d9120cd2bcbacf25b46db2c0fcb1e7a2
Ishtiak Ahmed <ishtiakahmed01999@gmail.com>
2024-04-19 16:49:36 +0600

    Home app section added to the home page

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Home/Home/Footer/Footer";
 
 
 const MainLayout = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
---
+++ src/Pages/Home/Home/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+
+
+const Footer = () => {
+    return (
+        <div>
+            <footer className="footer h-[400px] p-10 bg-[#3A643B2E] text-base-content flex justify-center items-center">
+                <div>
+                    <h6 className="footer-title font-bold text-[#3A643B]">Information</h6>
+                    <h1 className="text-[#474747] cursor-pointer text-base">About Us</h1>
+                    <h1 className="text-[#474747] cursor-pointer text-base">Terms and Conditions</h1>
+                    <h1 className="text-[#474747] cursor-pointer text-base">Privacy Policy</h1>
+                    <h1 className="text-[#474747] cursor-pointer text-base">Privacy Policy for Mobile Apps</h1>
+                    <h1 className="text-[#474747] cursor-pointer text-base">Shipping and Returns Policy</h1>
+                    <h1 className="text-[#474747] cursor-pointer text-base">International Delivery</h1>
+                    <h1 className="text-[#474747] cursor-pointer text-base">For Businesses, Hotels and Resorts</h1>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Footer;
